dd1d5b8c52f7034c9556637324be9d3ae0cc1b9b
zainebkhlifi1 <zaineb.khlifi@esprit.tn>
2025-07-19 11:22:02 +0100

    add eurika server

--- Restaurant/src/main/java/com/example/restaurant/entities/Restaurant.java
+++ Restaurant/src/main/java/com/example/restaurant/entities/Restaurant.java
@@ -1,16 +1,18 @@
 package com.example.restaurant.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 @Entity
@@ -20,6 +22,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@SpringBootApplication
 public class Restaurant implements Serializable {
 //    @Indexed
     @Setter(AccessLevel.MODULE)
--- Restaurant/src/main/java/com/example/restaurant/repositories/RestaurantRepository.java
+++ Restaurant/src/main/java/com/example/restaurant/repositories/RestaurantRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-//public interface RestaurantRepository extends MongoRepository<Restaurant, String> {
     public interface RestaurantRepository extends JpaRepository<Restaurant, String> {
 
     Optional<Restaurant> findByName(String name);
